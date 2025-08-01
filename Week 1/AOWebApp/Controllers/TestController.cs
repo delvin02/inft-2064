773cd38e4290e60a3ad4036d682220017fa58676
khowy006 <khowy006@mymail.unisa.edu.au>
2025-08-01 21:09:38 +0930

    dev: week 1 - form & understanding mvc and routing

---
+++ Week 1/AOWebApp/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AOWebApp.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: TestController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RazorTest(int id, string query, string formValue)
+        {
+            ViewBag.Id = id;
+            ViewBag.Query = query;
+            ViewBag.FormValue = formValue;
+            return View();
+        }
+    }
+}
