1496d14fab65dbcd70348901ac1ce834ac195d4e
khowy006 <khowy006@mymail.unisa.edu.au>
2025-08-09 21:35:30 +0930

    week 3: activity & pracs done

--- Week 3/AOWebApp/AOWebApp/Models/Customer.cs
+++ Week 3/AOWebApp/AOWebApp/Models/Customer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AOWebApp.Models
 {
@@ -18,6 +20,23 @@ namespace AOWebApp.Models
         public string MainPhoneNumber { get; set; } = null!;
         public string? SecondaryPhoneNumber { get; set; }
         public int? AddressId { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Customer Name")]
+        public string FullName => FirstName + " " + LastName;
+
+        [NotMapped]
+        [Display(Name = "Contact Number")]
+        public string ContactNumber
+        {
+            get
+            {
+                var contact = "";
+                if (!string.IsNullOrWhiteSpace(MainPhoneNumber)) { contact = MainPhoneNumber;  }
+                if (!string.IsNullOrWhiteSpace(SecondaryPhoneNumber)) { contact += (contact.Length > 0 ? "<br />" : "") + SecondaryPhoneNumber; }
+                return contact;
+            }
+        }
 
         public virtual Address? Address { get; set; }
         public virtual ICollection<CustomerOrder> CustomerOrders { get; set; }
